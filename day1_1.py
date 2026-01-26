87b7b5e8bc8ece8ee9bd72e9016570f670fae3ae
Alin <alinvagatu@gmail.com>
2026-01-26 17:30:44 +0200

    Initial commit

---
+++ day1_1.py
@@ -0,0 +1,2 @@
+# display a message
+print("Hello Word")
---
+++ day1_2.py
@@ -0,0 +1,18 @@
+# init variables
+integer_var = 1
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3]
+tuple_var = (4,5,6)
+dictionar_var = {"name": "Alin", "age": 42}
+boolean_var = True
+
+# display variable values
+print("Integer variable ", integer_var)
+print('Float var ', float_var)
+print('String ', string_var + " ccccc")
+list_var.append(111)
+print("list ", list_var)
+print('tuple ', tuple_var)
+print('dictionary name ', dictionar_var['name'])
+print('boolean ', boolean_var)
